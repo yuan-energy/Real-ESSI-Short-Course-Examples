113704c5089b687c0360c94b71d797aa97b7886c
Yuan <fengyuan.energy@gmail.com>
2017-11-22 10:10:48 -0800

    updated scripts.

---
+++ short-course-examples/nonlinear_analysis_steps/free_field_1D/vonMisesGoverGmax/run_plot_results.sh
@@ -0,0 +1,9 @@
+
+python extract_node_disp.py shell_structure_motion.h5.feioutput 5 x
+
+python extract_node_acce.py shell_structure_motion.h5.feioutput 5 x
+
+python extract_node_spectrum.py shell_structure_motion.h5.feioutput 5 x
+
+
+
