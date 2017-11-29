1dc1cb99f17f0b6f523877e3662d664bd9d60ce0
Yuan <fengyuan.energy@gmail.com>
2017-11-28 18:16:01 -0800

    updated motion .

--- short-course-examples/Day2/Deconvolution_1D_Motions/tar_leaf_dir.sh
+++ short-course-examples/Day2/Deconvolution_1D_Motions/tar_leaf_dir.sh
@@ -18,7 +18,7 @@ for element in $(seq 0 $((${#deepest_dir_array[@]} - 1)))
 do
 	# cp run_parallel_simulation.sh ${current_dir}/"${deepest_dir_array[$element]}"
 	cd ${current_dir}
-	cp run_sequential_simulation.sh ${current_dir}/"${deepest_dir_array[$element]}"
+	# cp run_sequential_simulation.sh ${current_dir}/"${deepest_dir_array[$element]}"
 	# cp extract_node_acce.py ${current_dir}/"${deepest_dir_array[$element]}"
 	# cp extract_node_disp.py ${current_dir}/"${deepest_dir_array[$element]}"
 	# cp extract_node_spectrum.py ${current_dir}/"${deepest_dir_array[$element]}"
