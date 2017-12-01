6a1134fa10eb50c275de1ff560cdf7b991dba112
Yuan <fengyuan.energy@gmail.com>
2017-11-30 18:32:44 -0800

    updated structure only motion.

--- short-course-examples/nonlinear_analysis_steps/structure/imposed_motion/run_plot_compare.sh
+++ short-course-examples/nonlinear_analysis_steps/structure/imposed_motion/run_plot_compare.sh
@@ -1,6 +1,6 @@
 
 
-python compare_top_acc.py sw4_free_field_center_ax.txt DRM1D_motion_node_5_x_acce.txt
+python compare_top_acc.py sw4_free_field_center_ax.txt shell_structure_imposed_motion_node_38_x_acce.txt
 
 
 
--- short-course-examples/nonlinear_analysis_steps/structure/imposed_motion/run_plot_results.sh
+++ short-course-examples/nonlinear_analysis_steps/structure/imposed_motion/run_plot_results.sh
@@ -3,7 +3,9 @@ python extract_node_disp.py shell_structure_imposed_motion.h5.feioutput 38 x
 
 python extract_node_acce.py shell_structure_imposed_motion.h5.feioutput 38 x
 
-python extract_node_spectrum.py shell_structure_imposed_motion.h5.feioutput 38 x
+python extract_node_spectrum_in_freq.py shell_structure_imposed_motion.h5.feioutput 38 x
+
+python extract_node_spectrum_in_period.py shell_structure_imposed_motion.h5.feioutput 38 x
 
 
 
