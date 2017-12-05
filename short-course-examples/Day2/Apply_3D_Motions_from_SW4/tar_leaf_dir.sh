adc711114d821241f1737861bd16ad02cee17ae8
Yuan <fengyuan.energy@gmail.com>
2017-12-04 20:52:56 -0800

    updated motions.

---
+++ short-course-examples/Day2/Apply_3D_Motions_from_SW4/tar_leaf_dir.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# ********************************************************************
+# Author: Yuan Feng
+# Date: Sun Sep 24 12:00:55 PDT 2017
+# Comments: 
+# 	1. This script will compress the complete files in each example
+# 	   to one compressed tgz file in each example subfolder, 
+#      such that we will have a downloadable link for each example. 
+#   2. The downloadable link is then used in example documentation in
+#      Lecture_notes or other documentation.
+# ********************************************************************
+
+
+current_dir=${PWD}
+deepest_dir_array=( $(find . -type d -links 2 ) )
+
+for element in $(seq 0 $((${#deepest_dir_array[@]} - 1)))
+do
+	cd ${current_dir}/"${deepest_dir_array[$element]}"
+	echo $PWD
+	sed -i 's/unit\_of\_Vs\ =\ 1/unit\_of\_Vs\_or\_Vp\ =\ 1/' main.fei
+	sed -i 's/enable\ acceleration\ output\;//' main.fei
+	rm -f *.tgz
+	tar -czvf ${PWD##*/}.tgz *
+
+done
--- short-course-examples/Day2/Deconvolution_1D_Motions/tar_leaf_dir.sh
+++ short-course-examples/Day2/Deconvolution_1D_Motions/tar_leaf_dir.sh
@@ -16,49 +16,11 @@ deepest_dir_array=( $(find . -type d -links 2 ) )
 
 for element in $(seq 0 $((${#deepest_dir_array[@]} - 1)))
 do
-	cd ${current_dir}
-	
-	# cp run_sequential_simulation.sh ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp run_parallel_simulation.sh ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp run_plot_results.sh ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp extract_node_acce.py ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp extract_node_disp.py ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp extract_node_spectrum.py ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp README.md ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp README.html ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp geometry_slice.png ${current_dir}/"${deepest_dir_array[$element]}"
-	# cp sw4_free_field_center*.txt ${current_dir}/"${deepest_dir_array[$element]}"
-
-
-	# cp base_correct_*.txt ${current_dir}/"${deepest_dir_array[$element]}"
-
 	cd ${current_dir}/"${deepest_dir_array[$element]}"
-	# sed -i 's/acceleration_filename\ =\ \"sw4_free_field_center_ax\.txt\"/acceleration_filename\ =\ \"base_correct_x_acc\.txt\"/' main.fei
-	# sed -i 's/acceleration_filename\ =\ \"sw4_free_field_center_ay\.txt\"/acceleration_filename\ =\ \"base_correct_y_acc\.txt\"/' main.fei
-	# sed -i 's/acceleration_filename\ =\ \"sw4_free_field_center_az\.txt\"/acceleration_filename\ =\ \"base_correct_z_acc\.txt\"/' main.fei
-
-	# sed -i 's/displacement_filename\ =\ \"sw4_free_field_center_ux\.txt\"/displacement_filename\ =\ \"base_correct_x_dis\.txt\"/' main.fei
-	# sed -i 's/displacement_filename\ =\ \"sw4_free_field_center_uy\.txt\"/displacement_filename\ =\ \"base_correct_y_dis\.txt\"/' main.fei
-	# sed -i 's/displacement_filename\ =\ \"sw4_free_field_center_uz\.txt\"/displacement_filename\ =\ \"base_correct_z_dis\.txt\"/' main.fei
-
-	# sed -i 's/213\.200716355\ \ \ 2000\ \ \ \ \ 0\.03\ \ \ 100/500\ \ \ \ \ \ \ \ \ \ \ \ \ 2000\ \ \ \ \ 0\.03\ \ \ 100/' soil_profile.txt
-	# sed -i 's/319\.801074533\ \ \ 2000\ \ \ \ \ 0\.03\ \ \ 100/750\ \ \ \ \ \ \ \ \ \ \ \ \ 2000\ \ \ \ \ 0\.03\ \ \ 100/' soil_profile_Vp.txt
-
-	# sed -i 's/^Soil\_Elastic\_Modulus\ =\ 2e8\*Pa;/Soil\_Elastic\_Modulus\ =\ 1\.1e9\*Pa;/' main.fei
-	# sed -i 's/^Elastic\ =\ 2e8\*N\/m\^2;/Elastic\ =\ 1\.1e9\*N\/m\^2;/' main.fei
-	# sed -i 's/add\_compensation\_time\ =\ 0\.5\ \*\ s/add\_compensation\_time\ =\ 0\.0\ \*\ s/' main.fei
-
-
-
-
-	# sed -i 's/time_step\ =\ 0\.01\*s\ ;/time_step\ =\ 0\.0419384905125\*s\ ;/' main.fei
-	# sed -i 's/simulate\ 3500\ steps\ using\ transient\ algorithm\ /simulate\ 240\ steps\ using\ transient\ algorithm\ /' main.fei
-
-	# sed -i 's/simulate\ 240\ steps\ using\ transient\ algorithm\ /simulate\ 210\ steps\ using\ transient\ algorithm\ /' main.fei
-	# rm -f damping.fei
-	# sed -i 's/include\ \"damping\.fei\";//' main.fei
-	
 	echo $PWD
+	sed -i 's/unit\_of\_Vs\ =\ 1/unit\_of\_Vs\_or\_Vp\ =\ 1/' main.fei
+	sed -i 's/enable\ acceleration\ output\;//' main.fei
 	rm -f *.tgz
 	tar -czvf ${PWD##*/}.tgz *
+
 done
